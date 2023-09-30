205ac7956161939f414d56cfa2f0baeb88626762
Gokhan Okur <gokhanokur241@gmail.com>
2023-09-30 16:43:23 +0300

    BookStore

---
+++ Week3/BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,113 @@
+﻿using BookStore.DBOperation;
+using Microsoft.AspNetCore.Mvc;
+using static BookStore.BooksOperations.CreateBook.CreateBookCommand;
+using BookStore.BooksOperations.CreateBook;
+using BookStore.BookOperations.GetBooks;
+using BookStore.BookOperations.GetBookDetail;
+using BookStore.BookOperations.UpdateBook;
+using static BookStore.BookOperations.UpdateBook.UpdateBookCommand;
+using BookStore.BookOperations.DeleteBook;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        private readonly BookStoreDbContext _context;
+
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            GetBooksQuery query = new GetBooksQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            BookDetailViewModel result;
+            try
+            {
+                GetBookDetailQuery query = new GetBookDetailQuery(_context);
+                query.BookId = id;
+                result = query.Handle();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
+        {
+
+            CreateBookCommand command = new CreateBookCommand(_context);
+            try
+            {
+                command.Model = newBook;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult UpdateBook(int id, [FromBody] UpdateBookModel updatedBook)
+        {
+            try
+            {
+                UpdateBookCommand command = new UpdateBookCommand(_context);
+                command.BookId = id;
+                command.Model = updatedBook;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteBook(int id)
+        {
+            try
+            {
+                DeleteBookCommand command = new DeleteBookCommand(_context);
+                command.BookId = id;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+            
+        }
+
+
+
+
+
+    }
+}
---
+++ Week3/BookStore/BookStore/DbOperation/BookStoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BookStore.DBOperation;
+
+public class BookStoreDbContext : DbContext
+{
+    protected override void OnConfiguring
+   (DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase(databaseName: "BookStoreDB");
+    }
+    public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+    { }
+    public DbSet<Book> Books { get; set; }
+
+    public List<Book> GetBooksFromDatabase()
+    {
+        return Books.ToList();
+    }
+
+}
